b432021395950baa6e1fb7a71545c77c21b6a0a4
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-13 23:36:59 +0530

    accepted arguments with cargo run, and used the .nth() method

--- cli_calculator/src/main.rs
+++ cli_calculator/src/main.rs
@@ -1,3 +1,16 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    // the nth method return the nth index element of an iterator 
+    // and then advances it 
+    // similar to <list>.pop(n) in python
+    // it calls .next() on the iterator at the nth index
+    let operand1 = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let operand2 = args.nth(0).unwrap();
+    println!("{} {} {}", operand1, operator, operand2);
+    // args are supplied by ```cargo run -- <argument>```
+
 }
