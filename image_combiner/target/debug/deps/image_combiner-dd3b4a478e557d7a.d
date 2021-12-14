80ac9e266ee712d785fdf23d6f17ebb5d4a00121
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-14 19:22:06 +0530

    removed target folder

--- image_combiner/target/debug/deps/image_combiner-dd3b4a478e557d7a.d
+++
@@ -1,5 +0,0 @@
-/home/exosphere/Desktop/freecodecamp_rust_tutorial/image_combiner/target/debug/deps/image_combiner-dd3b4a478e557d7a: src/main.rs
-
-/home/exosphere/Desktop/freecodecamp_rust_tutorial/image_combiner/target/debug/deps/image_combiner-dd3b4a478e557d7a.d: src/main.rs
-
-src/main.rs:
--- image_combiner/target/debug/image_combiner.d
+++
@@ -1,1 +0,0 @@
-/home/exosphere/Desktop/freecodecamp_rust_tutorial/image_combiner/target/debug/image_combiner: /home/exosphere/Desktop/freecodecamp_rust_tutorial/image_combiner/src/main.rs
