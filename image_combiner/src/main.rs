0963b736801ab93a6da20a71fa58c36ad47292f0
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-15 23:17:36 +0530

    return (image, image_format) as tuple

--- image_combiner/src/main.rs
+++ image_combiner/src/main.rs
@@ -1,3 +1,16 @@
+mod arguments;
+use image::{io::Reader, DynamicImage, ImageFormat};
+use std::{io::BufReader, fs::File};
+
 fn main() {
-    println!("Hello, world!");
+    let args = arguments::Args::new();
+    let (image_1, image_format_1) = find_image(args.image_1);
+    let (image_2, image_format_2) = find_image(args.image_2);
+}
+
+fn find_image(path:String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap(); 
+    let image_format = image_reader.format().unwrap();
+    let image = image_reader.decode().unwrap();
+    (image, image_format)
 }
