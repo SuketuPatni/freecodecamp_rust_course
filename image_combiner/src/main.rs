e1d0bc1d799484597d7443a8ac4ca15b54c29134
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-16 22:56:46 +0530

    added get_smallest_dimension function

--- image_combiner/src/main.rs
+++ image_combiner/src/main.rs
@@ -2,10 +2,20 @@ mod arguments;
 use image::{io::Reader, DynamicImage, ImageFormat};
 use std::{io::BufReader, fs::File};
 
-fn main() {
+enum ImageDataError {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataError> {
     let args = arguments::Args::new();
     let (image_1, image_format_1) = find_image(args.image_1);
     let (image_2, image_format_2) = find_image(args.image_2);
+
+    if image_1_format != image_2_format {
+        Err(ImageDataError::DifferentImageFormats)
+    } 
+
+    Ok(())
 }
 
 fn find_image(path:String) -> (DynamicImage, ImageFormat) {
@@ -13,4 +23,11 @@ fn find_image(path:String) -> (DynamicImage, ImageFormat) {
     let image_format = image_reader.format().unwrap();
     let image = image_reader.decode().unwrap();
     (image, image_format)
+}
+
+fn get_smallest_dimensions(dim1: (u32,u32), dim2: (u32,u32)) -> (u32,u32) {
+    match dim1.0 * dim1.1 <= dim2.0 * dim2.1 {
+        true => dim1,
+        false => dim2
+    }
 }
